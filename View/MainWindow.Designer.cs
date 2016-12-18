f4090b8a48fef0d8710886ea751fd51dda82b96e
quartermeat <jeremy.williamson83@gmail.com>
2016-12-18 14:20:45 -0500

    initial commit

---
+++ View/MainWindow.Designer.cs
@@ -0,0 +1,101 @@
+﻿using System.Windows.Forms;
+
+namespace B2J
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 12);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(114, 29);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "BITMAP Files (*.bmp)|*.bmp";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.Title = "Please select an bitmap file to encode to jpeg.";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(12, 153);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(102, 33);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.AutoSize = true;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(12, 47);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel.TabIndex = 3;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(232, 203);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "MainWindow";
+            this.Text = "B2J";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private Button saveButton;
+        private FlowLayoutPanel flowLayoutPanel;
+    }
+}
+
---
+++ View/MainWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using B2J.Model;
+
+namespace B2J
+{
+    public partial class MainWindow : Form
+    {
+        private List<string> fileNamesList;
+        private List<PictureBox> pictureBoxList; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            
+            fileNamesList = new List<string>();
+            pictureBoxList = new List<PictureBox>();
+
+            int index = 0;
+            foreach (var currentFile in openFileDialog.FileNames)
+            {
+                using (var image = new Bitmap(currentFile))
+                {
+                    fileNamesList.Add(currentFile);
+                    var currentPictureBox = new PictureBox();
+                    currentPictureBox.Name = "pictureBox" + index;
+                    currentPictureBox.Size = new Size(49, 33);
+                    currentPictureBox.TabIndex = index;
+                    currentPictureBox.TabStop = false;
+                    currentPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    currentPictureBox.Image = image;
+                    
+                    pictureBoxList.Add(currentPictureBox);
+                    flowLayoutPanel.Controls.Add(currentPictureBox);
+                    index++;
+                }    
+            }
+
+            saveButton.Enabled = true;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string fileNameStringList = "";
+
+            for(int i = 0; i < fileNamesList.Count; i++)
+            {
+                string saveName = Path.GetDirectoryName(fileNamesList[i]) + @"\" + Path.GetFileNameWithoutExtension(fileNamesList[i]) + ".jpeg";
+                pictureBoxList[i].Image.SaveJpeg(saveName, 100);
+                fileNameStringList += saveName + "\n";
+            }
+            
+            MessageBox.Show("Saved images as: \n" + fileNameStringList);
+        }
+
+    }
+}
